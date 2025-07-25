76796b7e86023c622cb035c5de9065af45be1b61
Tonye Jack <jtonye@ymail.com>
2025-07-25 13:31:56 -0600

    Updated script

--- rust-install.sh
+++ rust-install.sh
@@ -29,7 +29,7 @@ else
 fi
 
 ## Add v to the version if it doesn't have it
-if [[ $INPUT_ADD_PREFIX_TO_VERSION == true  && $HAS_V_IN_VERSION == true && $VERSION != "v$VERSION" ]]; then
+if [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "true"  && "$HAS_V_IN_VERSION" == true && "$VERSION" != "v$VERSION" ]]; then
   VERSION="v$VERSION"
 fi
 
