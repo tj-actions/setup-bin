eab3213c941aa20d07785c707c24c6bbf2044562
Tonye Jack <jtonye@ymail.com>
2025-07-25 13:40:28 -0600

    Updated script

--- rust-install.sh
+++ rust-install.sh
@@ -26,9 +26,6 @@ else
   VERSION="$INPUT_VERSION"
 fi
 
-echo "VERSION: $VERSION"
-echo "INPUT_ADD_PREFIX_TO_VERSION: $INPUT_ADD_PREFIX_TO_VERSION"
-
 if [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "true" && "$VERSION" != "v*" ]]; then
   VERSION="v$VERSION"
 elif [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "false" && "$VERSION" == "v*" ]]; then
