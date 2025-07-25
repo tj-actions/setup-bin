aed0ce41be86df764486b17b12fcc4021b2f7ee7
Tonye Jack <jtonye@ymail.com>
2025-07-25 13:36:31 -0600

    Updated script

--- rust-install.sh
+++ rust-install.sh
@@ -28,13 +28,14 @@ else
   VERSION="$INPUT_VERSION"
 fi
 
-echo "HAS_V_IN_VERSION: $HAS_V_IN_VERSION"
-echo "VERSION: $VERSION"
-echo "INPUT_ADD_PREFIX_TO_VERSION: $INPUT_ADD_PREFIX_TO_VERSION"
-
-## Add v to the version if it doesn't have it
 if [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "true"  && "$HAS_V_IN_VERSION" == true && "$VERSION" != "v$VERSION" ]]; then
   VERSION="v$VERSION"
+elif [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "true"  && "$HAS_V_IN_VERSION" == false && "$VERSION" != "v$VERSION" ]]; then
+  VERSION="v$VERSION"
+elif [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "false"  && "$HAS_V_IN_VERSION" == true && "$VERSION" == "v$VERSION" ]]; then
+  VERSION="${VERSION#"v"}"
+elif [[ "$INPUT_ADD_PREFIX_TO_VERSION" == "false"  && "$HAS_V_IN_VERSION" == false && "$VERSION" == "v$VERSION" ]]; then
+  VERSION="${VERSION#"v"}"
 fi
 
 echo "VERSION: $VERSION"
